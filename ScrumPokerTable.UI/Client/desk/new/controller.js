fe343a9e891e98231d4484c2cc8d9a2268f882d5
Roman Golovanov <roman.golovanov@gmail.com>
2017-01-24 09:08:38 +0300

    fix natural number's table

--- ScrumPokerTable.UI/Client/desk/new/controller.js
+++ ScrumPokerTable.UI/Client/desk/new/controller.js
@@ -5,7 +5,7 @@
             "$scope", "$location", "DeskHubService", function($scope, $location, deskHubService) {
 
                 $scope.deskTypes = {
-                    "Natural numbers": ["?", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"],
+                    "Natural numbers": ["?", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "21", "28", "35", "42"],
                     "Fibonacchi numbers": ["?", "1", "2", "3", "5", "8", "13", "20", "40"]
                 };
                 $scope.state = {
